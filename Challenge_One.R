8c660abd3784cfe7f70b34d4423aad390a29d4f7
rapidsiege <54790794+rapidsiege@users.noreply.github.com>
2020-02-26 09:35:36 +0100

    Q4
    Change "dir" for the directory location of the .csv file. Only machine_data.csv is necessary for Q4.

--- Challenge_One.R
+++ Challenge_One.R
@@ -4,3 +4,25 @@ if (!is.null(parent.frame(2)$ofile)) {
 }
 
 
+## Load library ----
+
+library(data.table)
+library(lsr)
+library(ggplot2)
+
+# Read dataset ----
+
+data = fread('dir/machine_data.csv',
+          dec=".", sep=",") 
+
+# Question ----
+
+## 4. [2 points] According to the following boxplot, what is the median number of hotels in the machine area? 
+
+# Boxplot ----
+
+boxplot(data$num_hotels, data = data, ylim = c(0,2))
+
+# Answer ----
+
+## The median number of hotels in the machine area is 0.
